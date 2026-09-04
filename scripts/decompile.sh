70340e8afccebea1ebc052adcd43d25bd19e20dd
Ecl <142730570+EclCoder@users.noreply.github.com>
2026-09-04 10:48:15 +0700

    Update decompile.sh

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,1 +1,16 @@
-#!bin/bash
+#!/bin/bash
+set -e
+mkdir -p app/
+for apk in *.apk; do
+  [ -e "$apk" ] || continue
+
+  name="${apk%.apk}"
+
+  echo "=== Decompiling $apk code ==="
+  jadx --no-res -d "app/" "$apk"
+
+  echo "=== Decompiling $apk res ==="
+  apktool d "$apk" -o "app/" -f --no-src
+done
+
+echo "=== Done ==="
