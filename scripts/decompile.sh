4418885d58c969aad3d65249f3850cc665a4f924
Ecl <142730570+EclCoder@users.noreply.github.com>
2026-09-04 14:08:16 +0700

    Update decompile.sh

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 set -e
-mkdir -p app/
-for apk in *.apk; do
-  [ -e "$apk" ] || continue
+
+mkdir -p src/ res/
+
+echo "=== Decompiling base.apk code ==="
+
+jadx --no-res -d "src/base" base.apk || true
+
+echo "=== Decompiling base.apk res ==="
+apktool d base.apk -o "res/base" -f --no-src
+
+for apk in split_config.en.apk split_config.vi.apk; do
+  if [ -f "$apk" ]; then
+    name="${apk%.apk}"
+
+    echo "=== Decompiling $apk res ==="
+    apktool d "$apk" -o "res/$name" -f --no-src
+  fi
+done
+
+for apk in split_config.*dpi.apk; do
+  [ -f "$apk" ] || continue
 
   name="${apk%.apk}"
 
-  echo "=== Decompiling $apk code ==="
-  jadx --no-res -d "app/" "$apk"
-
   echo "=== Decompiling $apk res ==="
-  apktool d "$apk" -o "app/" -f --no-src
+  apktool d "$apk" -o "res/$name" -f --no-src
 done
-
-echo "=== Done ==="
